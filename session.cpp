f6c0a775d4bb8a84f303dffbfc73cccb1f57872e
lance <a18361351@163.com>
2025-06-25 13:46:30 +0000

    tlv msgnode + nethost byte order impl && receiver impl

--- session.cpp
+++ session.cpp
@@ -3,6 +3,57 @@
 
 using namespace boost::asio;
 
+void Session::Receiver() {
+    auto cb = [self = shared_from_this()](const errcode& err, std::size_t bytes_rcvd) {
+        if (err) {
+            // tell that error!
+            return;
+        }
+        if (bytes_rcvd == 0) {
+            // closing
+            self->sock_.close();
+            return;
+        }
+        
+        // cur_pos更新
+        self->recv_buf_.cur_pos_ += bytes_rcvd;
+
+        if (self->recv_buf_.cur_pos_ >= self->recv_buf_.ctx_len_ + HEAD_LEN) {
+            // 处理了一整个消息
+            uint32_t tag;
+
+            memcpy(&tag, self->recv_buf_.data_, TAG_LEN);
+            tag = ntohl(tag);
+            self->ReceiveHandler(self->recv_buf_.ctx_len_, tag);
+            // 处理完毕后清除缓冲区，准备下一次接收
+            self->recv_buf_.Zero();
+        } else if (self->recv_buf_.cur_pos_ >= HEAD_LEN) {
+            // 处理了报文长度部分
+            // 此时我们可以将ctx_len部分来获取出来了
+
+            memcpy(&self->recv_buf_.ctx_len_, self->recv_buf_.data_ + TAG_LEN, LENGTH_LEN);
+            self->recv_buf_.ctx_len_ = ntohl(self->recv_buf_.ctx_len_);
+
+            if (self->recv_buf_.ctx_len_ > MAX_CTX_LEN) {
+                // 超出最大报文长度了！
+                self->sock_.close();
+                return;
+            }
+            if (self->recv_buf_.ctx_len_ + HEAD_LEN > self->recv_buf_.max_len_) {
+                self->recv_buf_.Reallocate(self->recv_buf_.ctx_len_ + HEAD_LEN);
+            }
+        } 
+        self->Receiver();
+    };
+    // 规范消息头部
+    if (recv_buf_.cur_pos_ < HEAD_LEN) {
+        boost::asio::async_read(sock_, boost::asio::buffer(recv_buf_.data_ + recv_buf_.cur_pos_, HEAD_LEN - recv_buf_.cur_pos_), cb);
+    } else {
+        boost::asio::async_read(sock_, boost::asio::buffer(recv_buf_.data_ + recv_buf_.cur_pos_, recv_buf_.ctx_len_ + HEAD_LEN - recv_buf_.cur_pos_), cb);
+    }
+}
+
+
 // void Session::DoRecv() {
 //     // 由于异步回调捕获了self(shared_from_this)，回调完成之前Session对象不会被销毁
 //     auto self = shared_from_this();
--- session.hpp
+++ session.hpp
@@ -13,6 +13,7 @@
 #include <boost/uuid/random_generator.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <mutex>
+#include <stdexcept>
 
 class Server;
 
@@ -20,28 +21,100 @@ class Server;
 
 using errcode = boost::system::error_code;
 
+const int TAG_LEN = 4;
+const int LENGTH_LEN = 4;
+const int HEAD_LEN = TAG_LEN + LENGTH_LEN;
+const uint32_t MAX_CTX_LEN = 1024 * 1024;   // 消息长度上限，实际应用应该不会发送如此大的消息
+
 // 用来存储数据的MsgNode节点类
-//  TODO(future): 也许未来可以避免频繁的内存分配和释放？
+// 在其中定义一个TLV协议格式：
+//  | Tag(4字节) | Len(4字节) | Content(Len字节) |
 class MsgNode {
     friend class Session;
 public:
     // Ctors
     // 通过最大长度构造空节点对象
-    MsgNode(std::size_t max_len) : data_(max_len > 0 ? new char[max_len] : nullptr), cur_pos_(0), max_len_(max_len) {}
+    MsgNode(std::size_t max_len, uint32_t tag = 0) : 
+        data_(max_len > 0 ? new char[TAG_LEN + LENGTH_LEN + max_len] : nullptr), 
+        cur_pos_(0), 
+        ctx_len_(max_len) {
+        assert(TAG_LEN == sizeof(uint32_t));
+        assert(LENGTH_LEN == sizeof(uint32_t));
+        if (max_len > 0) {
+            uint32_t net_max_len = htonl(max_len);
+            uint32_t net_tag = htonl(tag);
+            memcpy(data_, &net_tag, TAG_LEN);
+            memcpy(data_ + TAG_LEN, &net_max_len, LENGTH_LEN);
+        } else {
+            throw std::invalid_argument("Attempt to create a zero-sized message");
+        }
+    }
     // 通过字符串和最大长度构造带有消息的节点对象，一次拷贝
-    MsgNode(const char* msg, std::size_t msg_len) : data_(msg_len > 0 ? new char[msg_len] : nullptr), cur_pos_(0), max_len_(msg_len) {
-        if (msg_len > 0)
-            memcpy(data_, msg, msg_len);
+    MsgNode(const char* msg, std::size_t msg_len, uint32_t tag = 0) : 
+        data_(msg_len > 0 ? new char[TAG_LEN + LENGTH_LEN + msg_len] : nullptr), 
+        cur_pos_(0), 
+        ctx_len_(msg_len) {
+        assert(TAG_LEN == sizeof(uint32_t));
+        assert(LENGTH_LEN == sizeof(uint32_t));
+        if (msg_len > 0) {
+            uint32_t net_msg_len = htonl(msg_len);
+            uint32_t net_tag = htonl(tag);
+            memcpy(data_, &net_tag, TAG_LEN);
+            memcpy(data_ + TAG_LEN, &net_msg_len, LENGTH_LEN);
+            memcpy(data_ + TAG_LEN + LENGTH_LEN, msg, msg_len);
+        } else {
+            throw std::invalid_argument("Attempt to create a zero-sized message");
+        }
     }
     ~MsgNode() {
         if (data_)
             delete[] data_;
     }
+
+    // 销毁缓冲区
+    void Clear() {
+        if (data_)
+            delete[] data_;
+        data_ = nullptr;
+        cur_pos_ = 0;
+        ctx_len_ = 0;
+        max_len_ = 0;
+    }
+
+    // 清空缓冲区
+    void Zero() {
+        cur_pos_ = 0;
+        ctx_len_ = 0;
+        // memset(data_, 0, max_len_); // 极端性能要求场景中，我们不需要真的去清零缓冲区
+    }
+
+    // 扩张缓冲区（应该叫Expand更好？），new_len参数为缓冲区本身长度（调用时应传入ctx_len+HEAD_LEN）
+    void Reallocate(uint32_t new_len) {
+        // new/delete实现中没有realloc()类似的函数，只能删了重新分配
+        // 扩展空间的情况
+        if (data_ && new_len > max_len_) {
+            char* tmp = new char[new_len];
+            memcpy(tmp, data_, max_len_);
+            delete[] data_;
+            data_ = tmp;
+        }
+        // data_为空的情况
+        if (!data_) {
+            data_ = new char[new_len];
+            cur_pos_ = 0;
+            ctx_len_ = 0;
+        }
+        max_len_ = std::max(max_len_, new_len);
+    }
+
+    
 private:
-    char* data_;
-    std::size_t cur_pos_;
-    std::size_t max_len_;
+    char* data_;    // 包括tag, ctx_len, data的缓冲区，注意tag和ctx_len为网络端字节序
+    uint32_t cur_pos_;  // 当前读取/写入的位置（包括头部在内）
+    uint32_t ctx_len_;  // 内容的长度（不包括头部）
+    uint32_t max_len_;  // 当前缓冲区的最大长度
 };
+
 
 
 class Session : public std::enable_shared_from_this<Session> {
@@ -66,28 +139,14 @@ public:
     // -----------------------
     // 读取时的回调操作
 public:
-    void ReceiveHandler(std::size_t bytes_rcvd);
+    // 
+    void ReceiveHandler(uint32_t content_len, uint32_t tag);
 
-    void Receiver() {
-        auto cb = [self = shared_from_this()](const errcode& err, std::size_t bytes_rcvd) {
-            if (err) {
-                // tell that error!
-                return;
-            }
-            if (bytes_rcvd == 0) {
-                // closing
-                return;
-            }
-            self->ReceiveHandler(bytes_rcvd);
-            self->Receiver();   // 不断接收信息
-        };
-        sock_.async_receive(boost::asio::buffer(recv_buf_.data_ + recv_buf_.cur_pos_, recv_buf_.max_len_ - recv_buf_.cur_pos_), cb);
-        // boost::asio::async_read(sock_, boost::asio::buffer(recv_buf_.data_ + recv_buf_.cur_pos_, recv_buf_.max_len_ - recv_buf_.cur_pos_), cb);
-    }
+    void Receiver();
 
 private:
     MsgNode recv_buf_{1024};
-
+    
     // -----------------------
 
     // -----------------------
