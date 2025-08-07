b75730f80aff171bd55c61ebae8bc83193216947
a18361351 <a18361351@163.com>
2025-08-07 18:13:08 +0800

    client cli impl

--- src/client/client_main.cpp
+++ src/client/client_main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -15,6 +16,7 @@
 
 #include "common/msgnode.hpp"
 #include "log/log_manager.hpp"
+#include "utils/field_op.hpp"
 #include "utils/util_class.hpp"
 
 using errcode = boost::system::error_code;
@@ -380,16 +382,62 @@ int main() {
         sess->Start(remote);
     });
     sess->Verify(uid, token);
-    std::string inp;
+    std::array<char, 255> inp;
+    size_t end_pos;
     while (true) {
-        cin >> inp;
+        cin.getline(inp.data(), inp.size());
+        end_pos = strlen(inp.data());
+        if (end_pos == 0) continue;
         if (!sess->Running()) {
             cout << "Connection closed\n";
             break;
         }
-        if (inp == "exit") break;
-        std::string buf;
-        sess->Send(inp.c_str(), inp.size(), DEBUG);
+        if (inp[0] == '\\') {
+            // command
+            string command; vector<string> args;
+            auto sep = find(inp.begin(), inp.begin() + end_pos, ' ');
+            if (sep < inp.begin() + end_pos) {
+                command = string(inp.begin() + 1, sep);
+                // args = std::split(string(sep + 1, inp.end()), ' ');
+                string arg_str(sep + 1, inp.begin() + end_pos);
+                size_t start = 0, end;
+                while ((end = arg_str.find(' ', start)) != string::npos) {
+                    args.push_back(arg_str.substr(start, end - start));
+                    start = end + 1;
+                }
+                if (start < arg_str.size()) {
+                    args.push_back(arg_str.substr(start));
+                }
+            } else {
+                command = string(inp.begin() + 1, inp.begin() + end_pos);
+            }
+            if (command == "send") {
+                if (args.size() != 2) {
+                    cout << "usage: send [whom by uid] [message]\n";
+                } else {
+                    uint64_t whom_uid;
+                    try {
+                        whom_uid = stoull(args[0]);
+                    } catch (...) {
+                        cout << "Invalid UID: " << args[0] << "\n";
+                        continue;
+                    }
+                    string msg_data(args[1].size() + sizeof(uint64_t), 0);
+                    WriteNetField64(msg_data.data(), whom_uid);
+                    memcpy(msg_data.data() + sizeof(uint64_t), args[1].c_str(), args[1].size());
+                    // 发送消息
+                    sess->Send(msg_data.data(), msg_data.size(), CHAT_MSG);
+                    cout << "Message sent to user with UID: " << whom_uid << "\n";
+                }
+            } else if (command == "exit") {
+                cout << "bye\n";
+                break;
+            } else {
+                cout << "Unknown command: " << command << "\n";
+            }
+        } else {
+            sess->Send(inp.data(), end_pos, DEBUG);
+        }
     }
     printf("Closing\n");
     sess->Close();
